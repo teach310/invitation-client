aa58611a1c4d771e8226f36ca43eecd46efd0c0b
Taichi Sato <okms310gmail.com>
2020-06-14 23:46:27 +0900

    feat post

--- docs/js/uihandler.js
+++ docs/js/uihandler.js
@@ -1,23 +1,23 @@
-const showMessage = () => {
-     const nameField = document.getElementById('nameField');
-     alert(nameField.value);
-}
-
-const onClickPost = () => {
+const onClickPost = async () => {
      const nameField = document.getElementById('nameField');
      const addressField = document.getElementById('addressField');
-     postAppendMember(nameField.value, addressField.value);
+     const res = await postAppendMember(nameField.value, addressField.value);
+     if (res.status == 200){
+          alert("メンバーを追加しました");
+     }else{
+          alert("メンバーの追加に失敗しました");
+     }
 }
 
 const API_URL = "https://script.google.com/macros/s/AKfycbwLCFDEBKj8xCjZ2QMwkcXQdQds49GcAATbdLoZ7WJFdEW_mkY4/exec";
 const postAppendMember = async (name, address) => {
-     const data = {name: name, address: address};
-     const method = "POST";
-     const body = JSON.stringify(data);
-     const headers = {
-          // 'Accept': 'application/json',
-          'Content-Type': 'application/json'
-     };
-     const res = await fetch(API_URL, {method, headers, body});
-     alert(res);
+     const data = new FormData();
+     data.set("name", name);
+     data.set("address", address)
+     
+     const res = await fetch(API_URL, {
+          method: "POST", 
+          body: data
+     });
+     return res;
 }
